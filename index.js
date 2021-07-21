8f33022f80b18b480623cb360dba1f3afa7bb046
周柏民 <zhoubaimin@hcoin.com>
2021-07-21 16:05:44 +0800

    Use Eventemitter.inherito; version backup 0.1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
  * @name: @zbm1/timer
- * @version: 1.0.1
+ * @version: 0.1.0
  * @description: A Timer class of javascript
  * @author: zbm2001@aliyun.com
  * @license: ISC
@@ -35,143 +35,130 @@ function recursionClear (clearMethod, t, k, o) {
   }
 }
 
-var Timer = /*@__PURE__*/(function (EventEmitter) {
-  function Timer () {
-    EventEmitter.apply(this, arguments);
-  }
+function Timer () {
+}
 
-  if ( EventEmitter ) Timer.__proto__ = EventEmitter;
-  Timer.prototype = Object.create( EventEmitter && EventEmitter.prototype );
-  Timer.prototype.constructor = Timer;
+Timer.TIMEOUTS_KEY = '_timeouts';
+Timer.INTERVALS_KEY = '_intervals';
 
-  Timer.prototype._clearTimer = function _clearTimer (sNamespace, clearMethod) {
-    var this$1 = this;
+EventEmitter.inherito(Timer, {
+    /**
+     * 终止清除 clearTimeout 或 clearInterval
+     * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 或 this[INTERVALS_KEY] 下
+     * @param {Function} clearMethod 指定 clearTimeout 或 clearInterval
+     */
+    _clearTimer: function _clearTimer (sNamespace, clearMethod) {
+      var this$1 = this;
 
-    var ct = {
-      name: Timer.TIMEOUTS_KEY,
-      clearMethod: clearTimeout
-    };
-    var ci = {
-      name: Timer.INTERVALS_KEY,
-      clearMethod: clearInterval
-    };
-    var arr = clearMethod === clearTimeout
-      ? [ct]
-      : clearMethod === clearInterval
-        ? [ci]
-        : [ct, ci];
+      var ct = {
+        name: Timer.TIMEOUTS_KEY,
+        clearMethod: clearTimeout
+      };
+      var ci = {
+        name: Timer.INTERVALS_KEY,
+        clearMethod: clearInterval
+      };
+      var arr = clearMethod === clearTimeout
+        ? [ct]
+        : clearMethod === clearInterval
+          ? [ci]
+          : [ct, ci];
 
-    var sNamespaces = sNamespace && sNamespace.trim();
-    sNamespaces && (sNamespaces = sNamespaces.split(/\s+/));
+      var sNamespaces = sNamespace && sNamespace.trim();
+      sNamespaces && (sNamespaces = sNamespaces.split(/\s+/));
 
-    var forEach = sNamespaces
-      ? function (ref) {
-        var name = ref.name;
-        var clearMethod = ref.clearMethod;
+      var forEach = sNamespaces
+        ? function (ref) {
+          var name = ref.name;
+          var clearMethod = ref.clearMethod;
 
-        return sNamespaces.forEach(function (sNamespace) {
-        recursionClear(clearMethod, namespace(this$1[name], sNamespace), sNamespace, this$1[name]);
-      });
-    }
-      : function (ref) {
-        var name = ref.name;
-        var clearMethod = ref.clearMethod;
+          return sNamespaces.forEach(function (sNamespace) {
+          recursionClear(clearMethod, namespace(this$1[name], sNamespace), sNamespace, this$1[name]);
+        });
+      }
+        : function (ref) {
+          var name = ref.name;
+          var clearMethod = ref.clearMethod;
 
-        return recursionClear(clearMethod, this$1[name]);
-    };
+          return recursionClear(clearMethod, this$1[name]);
+      };
 
-    arr.forEach(forEach);
-  };
+      arr.forEach(forEach);
+    },
 
-  /**
-   * 设置 setTimeout 或 setInterval
-   * @param {Function} setMethod 指定 setTimeout 或 setInterval
-   * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 或 this[INTERVALS_KEY] 下
-   * @param {Function} fn 指定执行函数
-   * @param {Number} delay 指定延时毫秒数
-   */
-  Timer.prototype._setTimer = function _setTimer (setMethod, sNamespace, fn, delay) {
-    var name = setMethod === setTimeout ? Timer.TIMEOUTS_KEY : Timer.INTERVALS_KEY;
-    // this[Timer.TIMEOUTS_KEY] = {} // setTimeout id map
-    // this[Timer.INTERVALS_KEY] = {} // setInterval id map
-    if (!this[name]) {
-      this[name] = {};
-    }
-    if (typeof sNamespace === 'function') {
-      delay = fn;
-      fn = sNamespace;
-      sNamespace = uuid();
-      // 先做清除同名定时器操作
-    } else {
-      var clearMethod = setMethod === setTimeout ? clearTimeout : clearInterval;
-      this._clearTimer(sNamespace, clearMethod);
-    }
-    return namespace(this[name], sNamespace, setMethod(fn, delay))
-  };
+    /**
+     * 设置 setTimeout 或 setInterval
+     * @param {Function} setMethod 指定 setTimeout 或 setInterval
+     * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 或 this[INTERVALS_KEY] 下
+     * @param {Function} fn 指定执行函数
+     * @param {Number} delay 指定延时毫秒数
+     */
+    _setTimer: function _setTimer (setMethod, sNamespace, fn, delay) {
+      var name = setMethod === setTimeout ? Timer.TIMEOUTS_KEY : Timer.INTERVALS_KEY;
+      // this[Timer.TIMEOUTS_KEY] = {} // setTimeout id map
+      // this[Timer.INTERVALS_KEY] = {} // setInterval id map
+      if (!this[name]) {
+        this[name] = {};
+      }
+      if (typeof sNamespace === 'function') {
+        delay = fn;
+        fn = sNamespace;
+        sNamespace = uuid();
+        // 先做清除同名定时器操作
+      } else {
+        var clearMethod = setMethod === setTimeout ? clearTimeout : clearInterval;
+        this._clearTimer(sNamespace, clearMethod);
+      }
+      return namespace(this[name], sNamespace, setMethod(fn, delay))
+    },
 
-  /**
-   * 设置延时器 setTimeout
-   * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 下
-   * @param {Function} fn 指定执行函数
-   * @param {Number} delay 指定延时毫秒数
-   */
-  Timer.prototype.setTimeout = function setTimeout$1 (sNamespace, fn, delay) {
-    return this._setTimer(setTimeout, sNamespace, fn, delay)
-  };
+    /**
+     * 设置延时器 setTimeout
+     * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 下
+     * @param {Function} fn 指定执行函数
+     * @param {Number} delay 指定延时毫秒数
+     */
+    setTimeout: function setTimeout$1 (sNamespace, fn, delay) {
+      return this._setTimer(setTimeout, sNamespace, fn, delay)
+    },
 
-  /**
-   * 设置定时器 setInterval
-   * @param {string} sNamespace 指定命名空间，属于 this[INTERVALS_KEY] 下
-   * @param {Function} fn 指定执行函数
-   * @param {Number} delay 指定延时毫秒数
-   */
-  Timer.prototype.setInterval = function setInterval$1 (sNamespace, fn, delay) {
-    return this._setTimer(setInterval, sNamespace, fn, delay)
-  };
+    /**
+     * 设置定时器 setInterval
+     * @param {string} sNamespace 指定命名空间，属于 this[INTERVALS_KEY] 下
+     * @param {Function} fn 指定执行函数
+     * @param {Number} delay 指定延时毫秒数
+     */
+    setInterval: function setInterval$1 (sNamespace, fn, delay) {
+      return this._setTimer(setInterval, sNamespace, fn, delay)
+    },
 
-  /**
-   * 终止清除延时器 clearTimeout
-   * @param {string} [sNamespace] 指定命名空间，属于 this[TIMEOUTS_KEY]，若未指定则终止清除全部
-   *                              可指定多个命名空间（用空格分割）
-   */
-  Timer.prototype.clearTimeout = function clearTimeout$1 (sNamespace) {
-    this._clearTimer(sNamespace, clearTimeout);
-  };
+    /**
+     * 终止清除延时器 clearTimeout
+     * @param {string} [sNamespace] 指定命名空间，属于 this[TIMEOUTS_KEY]，若未指定则终止清除全部
+     *                              可指定多个命名空间（用空格分割）
+     */
+    clearTimeout: function clearTimeout$1 (sNamespace) {
+      this._clearTimer(sNamespace, clearTimeout);
+    },
 
-  /**
-   * 终止清除定时器 clearInterval
-   * @param {string} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
-   *                              可指定多个命名空间（用空格分割）
-   */
-  Timer.prototype.clearInterval = function clearInterval$1 (sNamespace) {
-    this._clearTimer(sNamespace, clearInterval);
-  };
+    /**
+     * 终止清除定时器 clearInterval
+     * @param {string} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
+     *                              可指定多个命名空间（用空格分割）
+     */
+    clearInterval: function clearInterval$1 (sNamespace) {
+      this._clearTimer(sNamespace, clearInterval);
+    },
 
-  /**
-   * 终止清除定时器 clearInterval
-   * @param {string} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
-   *                              可指定多个命名空间（用空格分割）
-   */
-  Timer.prototype.clearAllTimer = function clearAllTimer (sNamespace) {
-    this._clearTimer();
-  };
-
-  return Timer;
-}(EventEmitter));
-
-Object.defineProperties(Timer, {
-  TIMEOUTS_KEY: {
-    get: function get () {
-      return '_timeouts'
+    /**
+     * 终止清除定时器 clearTimerAll
+     * @param {string|undefined} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
+     *                              可指定多个命名空间（用空格分割）
+     */
+    clearTimerAll: function clearTimerAll () {
+      this._clearTimer();
     }
   },
-  INTERVALS_KEY: {
-    get: function get () {
-      return '_intervals'
-    }
-  }
-});
-
-Object.assign(Timer, EventEmitter);
+  EventEmitter);
 
 exports.default = Timer;
--- src/core.js
+++ src/core.js
@@ -21,127 +21,116 @@ function recursionClear (clearMethod, t, k, o) {
   }
 }
 
-export default class Timer extends EventEmitter {
-  // private _timeouts = {}
-  // private _intervals = {}
-
-  /**
-   * 终止清除 clearTimeout 或 clearInterval
-   * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 或 this[INTERVALS_KEY] 下
-   * @param {Function} clearMethod 指定 clearTimeout 或 clearInterval
-   */
-  _clearTimer (sNamespace, clearMethod) {
-    const ct = {
-      name: Timer.TIMEOUTS_KEY,
-      clearMethod: clearTimeout
-    }
-    const ci = {
-      name: Timer.INTERVALS_KEY,
-      clearMethod: clearInterval
-    }
-    let arr = clearMethod === clearTimeout
-      ? [ct]
-      : clearMethod === clearInterval
-        ? [ci]
-        : [ct, ci]
-
-    let sNamespaces = sNamespace && sNamespace.trim()
-    sNamespaces && (sNamespaces = sNamespaces.split(/\s+/))
-
-    const forEach = sNamespaces
-      ? ({name, clearMethod}) => sNamespaces.forEach(sNamespace => {
-        recursionClear(clearMethod, namespace(this[name], sNamespace), sNamespace, this[name])
-      })
-      : ({name, clearMethod}) => recursionClear(clearMethod, this[name])
-
-    arr.forEach(forEach)
-  }
-
-  /**
-   * 设置 setTimeout 或 setInterval
-   * @param {Function} setMethod 指定 setTimeout 或 setInterval
-   * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 或 this[INTERVALS_KEY] 下
-   * @param {Function} fn 指定执行函数
-   * @param {Number} delay 指定延时毫秒数
-   */
-  _setTimer (setMethod, sNamespace, fn, delay) {
-    let name = setMethod === setTimeout ? Timer.TIMEOUTS_KEY : Timer.INTERVALS_KEY
-    // this[Timer.TIMEOUTS_KEY] = {} // setTimeout id map
-    // this[Timer.INTERVALS_KEY] = {} // setInterval id map
-    if (!this[name]) {
-      this[name] = {}
-    }
-    if (typeof sNamespace === 'function') {
-      delay = fn
-      fn = sNamespace
-      sNamespace = uuid()
-      // 先做清除同名定时器操作
-    } else {
-      let clearMethod = setMethod === setTimeout ? clearTimeout : clearInterval
-      this._clearTimer(sNamespace, clearMethod)
-    }
-    return namespace(this[name], sNamespace, setMethod(fn, delay))
-  }
-
-  /**
-   * 设置延时器 setTimeout
-   * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 下
-   * @param {Function} fn 指定执行函数
-   * @param {Number} delay 指定延时毫秒数
-   */
-  setTimeout (sNamespace, fn, delay) {
-    return this._setTimer(setTimeout, sNamespace, fn, delay)
-  }
-
-  /**
-   * 设置定时器 setInterval
-   * @param {string} sNamespace 指定命名空间，属于 this[INTERVALS_KEY] 下
-   * @param {Function} fn 指定执行函数
-   * @param {Number} delay 指定延时毫秒数
-   */
-  setInterval (sNamespace, fn, delay) {
-    return this._setTimer(setInterval, sNamespace, fn, delay)
-  }
-
-  /**
-   * 终止清除延时器 clearTimeout
-   * @param {string} [sNamespace] 指定命名空间，属于 this[TIMEOUTS_KEY]，若未指定则终止清除全部
-   *                              可指定多个命名空间（用空格分割）
-   */
-  clearTimeout (sNamespace) {
-    this._clearTimer(sNamespace, clearTimeout)
-  }
-
-  /**
-   * 终止清除定时器 clearInterval
-   * @param {string} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
-   *                              可指定多个命名空间（用空格分割）
-   */
-  clearInterval (sNamespace) {
-    this._clearTimer(sNamespace, clearInterval)
-  }
-
-  /**
-   * 终止清除定时器 clearInterval
-   * @param {string} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
-   *                              可指定多个命名空间（用空格分割）
-   */
-  clearAllTimer (sNamespace) {
-    this._clearTimer()
-  }
+export default function Timer () {
 }
 
-Object.defineProperties(Timer, {
-  TIMEOUTS_KEY: {
-    get () {
-      return '_timeouts'
+Timer.TIMEOUTS_KEY = '_timeouts'
+Timer.INTERVALS_KEY = '_intervals'
+
+EventEmitter.inherito(Timer, {
+    /**
+     * 终止清除 clearTimeout 或 clearInterval
+     * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 或 this[INTERVALS_KEY] 下
+     * @param {Function} clearMethod 指定 clearTimeout 或 clearInterval
+     */
+    _clearTimer (sNamespace, clearMethod) {
+      const ct = {
+        name: Timer.TIMEOUTS_KEY,
+        clearMethod: clearTimeout
+      }
+      const ci = {
+        name: Timer.INTERVALS_KEY,
+        clearMethod: clearInterval
+      }
+      let arr = clearMethod === clearTimeout
+        ? [ct]
+        : clearMethod === clearInterval
+          ? [ci]
+          : [ct, ci]
+
+      let sNamespaces = sNamespace && sNamespace.trim()
+      sNamespaces && (sNamespaces = sNamespaces.split(/\s+/))
+
+      const forEach = sNamespaces
+        ? ({name, clearMethod}) => sNamespaces.forEach(sNamespace => {
+          recursionClear(clearMethod, namespace(this[name], sNamespace), sNamespace, this[name])
+        })
+        : ({name, clearMethod}) => recursionClear(clearMethod, this[name])
+
+      arr.forEach(forEach)
+    },
+
+    /**
+     * 设置 setTimeout 或 setInterval
+     * @param {Function} setMethod 指定 setTimeout 或 setInterval
+     * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 或 this[INTERVALS_KEY] 下
+     * @param {Function} fn 指定执行函数
+     * @param {Number} delay 指定延时毫秒数
+     */
+    _setTimer (setMethod, sNamespace, fn, delay) {
+      let name = setMethod === setTimeout ? Timer.TIMEOUTS_KEY : Timer.INTERVALS_KEY
+      // this[Timer.TIMEOUTS_KEY] = {} // setTimeout id map
+      // this[Timer.INTERVALS_KEY] = {} // setInterval id map
+      if (!this[name]) {
+        this[name] = {}
+      }
+      if (typeof sNamespace === 'function') {
+        delay = fn
+        fn = sNamespace
+        sNamespace = uuid()
+        // 先做清除同名定时器操作
+      } else {
+        let clearMethod = setMethod === setTimeout ? clearTimeout : clearInterval
+        this._clearTimer(sNamespace, clearMethod)
+      }
+      return namespace(this[name], sNamespace, setMethod(fn, delay))
+    },
+
+    /**
+     * 设置延时器 setTimeout
+     * @param {string} sNamespace 指定命名空间，属于 this[TIMEOUTS_KEY] 下
+     * @param {Function} fn 指定执行函数
+     * @param {Number} delay 指定延时毫秒数
+     */
+    setTimeout (sNamespace, fn, delay) {
+      return this._setTimer(setTimeout, sNamespace, fn, delay)
+    },
+
+    /**
+     * 设置定时器 setInterval
+     * @param {string} sNamespace 指定命名空间，属于 this[INTERVALS_KEY] 下
+     * @param {Function} fn 指定执行函数
+     * @param {Number} delay 指定延时毫秒数
+     */
+    setInterval (sNamespace, fn, delay) {
+      return this._setTimer(setInterval, sNamespace, fn, delay)
+    },
+
+    /**
+     * 终止清除延时器 clearTimeout
+     * @param {string} [sNamespace] 指定命名空间，属于 this[TIMEOUTS_KEY]，若未指定则终止清除全部
+     *                              可指定多个命名空间（用空格分割）
+     */
+    clearTimeout (sNamespace) {
+      this._clearTimer(sNamespace, clearTimeout)
+    },
+
+    /**
+     * 终止清除定时器 clearInterval
+     * @param {string} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
+     *                              可指定多个命名空间（用空格分割）
+     */
+    clearInterval (sNamespace) {
+      this._clearTimer(sNamespace, clearInterval)
+    },
+
+    /**
+     * 终止清除定时器 clearTimerAll
+     * @param {string|undefined} [sNamespace] 指定命名空间，属于 this[INTERVALS_KEY] 下，若未指定则终止清除全部
+     *                              可指定多个命名空间（用空格分割）
+     */
+    clearTimerAll () {
+      this._clearTimer()
     }
   },
-  INTERVALS_KEY: {
-    get () {
-      return '_intervals'
-    }
-  }
-})
-
-Object.assign(Timer, EventEmitter)
+  EventEmitter)
